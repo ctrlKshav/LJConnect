8a6c08d933bad5c88f5b27adaf4fd6662d832cc0
Keshav <keshavm9978@gmail.com>
2024-03-27 13:49:51 +0530

    Events

---
+++ js/events.js
@@ -0,0 +1,53 @@
+﻿var slideIndex = 3;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    var prevButton = document.querySelector(".prev");
+    var nextButton = document.querySelector(".next");
+
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active","");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+
+    if(slideIndex === 1){
+        prevButton.style.pointerEvents = "none";
+        prevButton.style.display = "none";
+    }else{
+        prevButton.style.pointerEvents = "auto";
+        prevButton.style.display = "inline";
+    }
+
+    if(slideIndex === 3){
+        nextButton.style.pointerEvents = "none";
+        nextButton.style.display = "none";
+    }else{
+        nextButton.style.pointerEvents = "auto";
+        nextButton.style.display="inline";
+    }
+
+}
